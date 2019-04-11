a4a4ef27f4a960359a59426284962735e9c7f534
nate-fleming <nfleming4@gmail.com>
2019-04-11 15:46:53 -0500

    function javaScript

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+console.log('hello')
+
+let firstName = 'Brenda';
+let lastName = 'Long';
+
+console.log(`${firstName} ${lastName}`)
+
+function displayName(lastName, firstName) {
+    return `${firstName} ${lastName} is awesome`
+}
+
+let myNewName = displayName("cat", "fish");
+console.log('myNewName', myNewName);
+
